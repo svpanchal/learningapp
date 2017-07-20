60d9456ec8553411dfdc6c1aad3b21920787ff9f
Sorum Panchal <Sorum.Panchal@matrixres.com>
2017-07-20 11:53:44 -0400

    Creating deploy scripts attempt2

--- deploy.cmd
+++ deploy.cmd
@@ -88,34 +88,23 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
 
 :: 2. Select node version
 call :SelectNodeVersion
 
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%" ::maybe pushd instead of cd
-::  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  cd /dev/null :: not sure if this belongs here
-  popd
-)
-
-::4. Run npm build
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  cd "%DEPLOYMENT_SOURCE%"
-  ::eval ./node_modules/@angular/cli/bin/ng build
-  call :ExecuteCmd !NPM_CMD! run build  ::i think this accomplishes what the line above describes
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
-:: 1. KuduSync -- MOVING THIS TO LAST STEP, PER NOTES
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist/" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
 
